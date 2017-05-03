aeadfa9a5c5c60a6ac2805cc9156d6da1e1f1df2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-05-03 13:05:03 +0800

    Improved code.

--- burne.js
+++ burne.js
@@ -32,6 +32,9 @@
 			"file": "burne.js",
 			"module": "burne",
 			"author": "Richeve S. Bebedor",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"eMail": "richeve.bebedor@gmail.com",
 			"repository": "https://github.com/volkovasystems/burne.git",
 			"test": "burne-test.js",
